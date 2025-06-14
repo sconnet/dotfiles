bb205b47571504dd77c1f7d6188a8727027f5e54
Steve Connet <{ID+}>
2025-06-14 16:40:51 -0700

    Update fzf paths - homebrew moved

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -2,17 +2,17 @@
 
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-if [ -f /usr/local/opt/fzf/shell/key-bindings.zsh ]; then
-    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+if [ -f /opt/homebrew/opt/fzf/shell/key-bindings.zsh ]; then
+    source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 fi
